35d60ecc2c9ad05007b1b7190050f3b5e010f324
Gururaj Desai <gururajdesaimac2026@Gururajs-MacBook-Pro---2026.local>
2026-01-26 18:06:00 -0800

    added

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
     // Enable auto-heal behavior in tests/plugins when supported by tooling
     //autoHeal: true,
   },
